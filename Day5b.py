a332f2ddb8a3cbfad5dcc97a1a0e49558f38afdc
Lex Ruesink <lexruesink@live.nl>
2021-12-05 10:44:37 +0100

    Day 5

---
+++ Day5b.py
@@ -0,0 +1,51 @@
+from Helper import read_input
+
+input = read_input('inputs/day5.txt')
+
+
+def parse_line(line):
+    coords = line.split(' -> ')
+    p1 = [int(x) for x in coords[0].split(',')]
+    p2 = [int(x) for x in coords[1].split(',')]
+    return p1, p2
+
+
+positions = {}
+
+
+def increment_pos(key: tuple):
+    if key not in positions:
+        positions[key] = 1
+    else:
+        positions[key] += 1
+
+
+for input_line in input:
+    line = parse_line(input_line)
+
+    if line[0][0] == line[1][0]:
+        start = min(line[0][1], line[1][1])
+        end = max(line[0][1], line[1][1])
+        for i in range(start, end + 1):
+            increment_pos((line[0][0], i))
+    elif line[0][1] == line[1][1]:
+        start = min(line[0][0], line[1][0])
+        end = max(line[0][0], line[1][0])
+        for i in range(start, end + 1):
+            increment_pos((i, line[0][1]))
+    else:
+        dir_x = -1 if line[0][0] > line[1][0] else 1
+        dir_y = -1 if line[0][1] > line[1][1] else 1
+        steps = max(line[0][1], line[1][1]) - min(line[0][1], line[1][1])
+
+        x = line[0][0]
+        for y in range(line[0][1], line[1][1] + dir_y, dir_y):
+            increment_pos((x, y))
+            x += dir_x
+
+count = 0
+for value in positions.values():
+    if value >= 2:
+        count += 1
+
+print(count)
